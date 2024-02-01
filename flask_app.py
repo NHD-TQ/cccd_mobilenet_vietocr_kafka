ba1d27c41bb0e61e2d12cd90d5c2cf87f2d8d68a
DUC-TQ <nguyenducsd01@gmail.com>
2024-02-01 11:54:50 +0700

    update MongoDB

--- flask_app.py
+++ flask_app.py
@@ -17,6 +17,14 @@ from kafka import KafkaProducer
 from kafka import KafkaConsumer
 # Thư viện chuyển từ dạng bytes sang dictionary từ message kafka
 from ast import literal_eval
+import datetime
+from pymongo import MongoClient
+import json
+
+# Tạo cơ sở dữ liệu có chức năng lưu dữ file, dữ liệu user tải lên
+client = MongoClient('mongodb://localhost:27017/')
+db = client.web_cccd_database
+data = db.data
 
 
 
@@ -62,28 +70,40 @@ def upload():
         delete_files_in_directory(folder_image)
         # image.save(secure_filename(image.filename))
 
+        # Lưu hình ảnh đã tải lên vào database
+        cur_time = datetime.datetime.now()
+        data.insert_one({'time': cur_time, 'image': image, 'result': None})
+        all_data = data.find()
+        image = db.data.find_one({'time': cur_time})
+        # Query để lấy image ra chỉ để học thêm về MongoDB chứ điều này sẽ tốn time hơn
+
         # Đọc ảnh user upload
-        image = Image.open(io.BytesIO(image))
+        image = Image.open(io.BytesIO(image.get('image')))
         image.save("C:\\Users\\admin\OneDrive - ptit.edu.vn\\Desktop\\cccd_final\\static\\image_submited\\image.jpg")
         # Chuyển ảnh thành dạng byte sau đó gửi về model AI xử lý
         img_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         img_cv2 = cv2.resize(img_cv2, dsize=None, fx=1, fy=1)
         ret, buffer = cv2.imencode('.jpg', img_cv2)
+        # print( buffer.tobytes())
         p.send(topic_name, buffer.tobytes())
         p.flush()
         print("Hình ảnh đã được gửi đến AI model")
 
         for message in c:
             results = message.value
-            # return render_template('result.html', result=results)
+
             # Chuyển đổi từ dạng bytes (message bắn từ kafka thành dạng dictionary)
             results = literal_eval(results.decode('utf8'))
+            # convert result to string and update lại vào cơ sở dữ liệu
+            res_db = str(results)
+            db.data.update_one({'time': cur_time}, {"$set":{"result":res_db}})
+
             return render_template("result.html", results=results)
 
         # results = process_image()
         # Convert image về dạng numpy
-        np_img = asarray(image)
-        #
+        # np_img = asarray(image)
+
         # # Đưa vào process with AI
         # results = process_image(np_img, image)
         # return render_template('result.html', result = results)
